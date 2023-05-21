a60632f0cd3b5c327c4968100d44a8d473256182
Akmulla <d4n73@yandex.ru>
2023-05-21 12:04:33 +0300

    sdl init

--- Main.c
+++ Main.c
@@ -1,10 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <SDL.h>
 #include "defs.h"
 #include "structs.h"
+#include "graphics.h"
+
+struct App app;
 
 int main()
 {
+	if (InitializeGraphics(&app) < 0)
+	{
+		printf("Could not initialize graphics\n");
+		return -1;
+	}
+
+
 	printf("Init\n");
+
 	return 0;
 }
