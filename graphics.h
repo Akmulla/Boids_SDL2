a0433ff98ed5f50aac5d8a84a2da8980c4014c09
Akmulla <d4n73@yandex.ru>
2023-05-28 19:28:36 +0300

    graphics

---
+++ graphics.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "structs.h"
+
+int InitializeGraphics(struct App* app);
+
+int RenderFrame(struct App* app);
---
+++ structs.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <SDL.h>
+
+struct App {
+	SDL_Renderer* renderer;
+	SDL_Window* window;
+};
